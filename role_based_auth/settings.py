793c52fd2c77171888bea2b623daec445e044291
ahmedhomrani <contact.ahmedhomrani@gmail.com>
2024-04-08 03:20:55 +0100

    first commit

--- role_based_auth/settings.py
+++ role_based_auth/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'account',
     'technicien',
     'validateur',
-    'PMS',
     'lignes',
 
 ]
--- role_based_auth/urls.py
+++ role_based_auth/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
-    path("pms/", include('PMS.urls')),
     path("lignes/", include('lignes.urls')),
 ]
