ed4d3d4e68b394959ffe68adb2d652bf51d3a0ac
ahmedhomrani <contact.ahmedhomrani@gmail.com>
2024-04-21 00:05:42 +0100

    fix delete ligne

--- lignesAssignTo/views.py
+++ lignesAssignTo/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import LignesAssignto
@@ -13,6 +14,14 @@ class LignesAssigntoRetrieveAPIView(generics.RetrieveAPIView):
     queryset = LignesAssignto.objects.all()
     serializer_class = LignesAssigntoSerializer
     permission_classes = [IsAdminUser]
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        except Http404:
+            return Response({"message": "Resource not found"}, status=status.HTTP_200_OK)
 
 class LignesAssigntoCreateAPIView(generics.CreateAPIView):
     queryset = LignesAssignto.objects.all()
