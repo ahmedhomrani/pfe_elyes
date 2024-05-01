4c6a06f82c5956a8e2bd98c8f64e746497ba4029
ahmedhomrani <contact.ahmedhomrani@gmail.com>
2024-05-01 01:12:04 +0100

    technicien

--- technicien/serializers.py
+++ technicien/serializers.py
@@ -4,4 +4,4 @@ from lignesAssignTo.models import LignesAssignto
 class LignesAssigntoUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = LignesAssignto
-        fields = ['status', 'realisation_date', 'comment']
+        fields = ['status', 'realisation_date', 'comment', 'confirmed']
